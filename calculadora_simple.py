0e93ba90a34bee71d000200b6dad44a5d8c81dfb
a151015-alt <a151015@idelvalle.org>
2026-01-21 19:06:20 -0600

    Create calculadora_simple.py

---
+++ calculadora_simple.py
@@ -0,0 +1,18 @@
+def main():
+    print("Calculadora sencilla en Python")
+    
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    
+    suma = num1 + num2
+    resta = num1 - num2
+    multiplicacion = num1 * num2
+    
+    print("Resultados:")
+    print("Suma:", suma)
+    print("Resta:", resta)
+    print("Multiplicación:", multiplicacion)
+
+if __name__ == "__main__":
+    main()
+S
